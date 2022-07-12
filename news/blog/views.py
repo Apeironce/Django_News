f905f0690d6be36401ae5f7872b4e77d9e4d849d
Apeironce <102956925+Apeironce@users.noreply.github.com>
2022-07-13 01:00:13 +0400

    Добавлены регистрация и аутентификация Переписаны некоторые методы в классы представлений

--- news/blog/views.py
+++ news/blog/views.py
@@ -1,26 +1,68 @@
+from django.contrib.auth import logout, login
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, DetailView
 
-from blog.forms import AddPostForm
+from blog.forms import AddPostForm, RegisterUserForm, LoginUserForm
 from blog.models import *
+from blog.utils import DataMixin
 
-menu = [{'title': 'Главная', 'url_name': 'home'},
-        {'title': 'О сайте', 'url_name': 'about'},
+menu = [{'title': 'О сайте', 'url_name': 'about'},
         {'title': 'Добавить статью', 'url_name': 'add_page'},
-        {'title': 'Войти', 'url_name': 'login'}
+        {'title': 'Обратная связь', 'url_name': 'add_page'},
         ]
 
 
-def home(request):
-    posts = Post.objects.all()
-    context = {
-        'menu': menu,
-        'posts': posts,
-        'title': 'Главная страница',
-        'cat_selected': 0,
-    }
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'blog/register.html'
+    success_url = reverse_lazy('login')
 
-    return render(request, 'blog/home.html', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+        return dict(list(context.items())+list(c_def.items()))
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'blog/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Авторизация')
+        return dict(list(context.items())+list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+
+class Home(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Главная страница'
+        context['cat_selected'] = 0
+        return context
+
+    def get_queryset(self):
+        return Post.objects.filter(is_published=True)
 
 
 def about(request):
@@ -39,43 +81,85 @@ def archive(request, year):
     return render(request, 'blog/archive.html', {'menu': menu, 'year': year, 'title': f'Архив за {year} год'})
 
 
-def show_category(request, cat_id):
-    posts = Post.objects.filter(cat_id=cat_id)
-    context = {
-        'menu': menu,
-        'posts': posts,
-        'title': 'Отображение по категориям',
-        'cat_selected': cat_id,
-    }
+class PostCategory(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    allow_empty = False
 
-    return render(request, 'blog/home.html', context=context)
+    def get_queryset(self):
+        return Post.objects.filter(cat_slug=self.kwargs['cat_slug'], is_published=True)
 
-
-def show_post(request, post_slug):
-    post = get_object_or_404(Post, pk=post_slug)
-    context = {
-        'menu': menu,
-        'posts': post,
-        'title': post.title,
-        'cat_selected': post.cat_id,
-    }
-
-    return render(request, 'blog/post.html', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
+        context['menu'] = menu
+        context['cat_selected'] = context['posts'][0].cat_id
+        return context
 
 
-def addpage(request):
-    if request.method == 'POST':
-        form = AddPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = AddPostForm()
-    return render(request, 'blog/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
+# def show_category(request, cat_id):
+#     posts = Post.objects.filter(cat_id=cat_id)
+#     context = {
+#         'menu': menu,
+#         'posts': posts,
+#         'title': 'Отображение по категориям',
+#         'cat_selected': cat_id,
+#     }
+#
+#     return render(request, 'blog/home.html', context=context)
 
 
-def login(request):
-    return HttpResponse("Авторизация")
+class ShowPost(DataMixin, DetailView):
+    model = Post
+    template_name = 'blog/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = context['post']
+        context['menu'] = menu
+        return context
+
+
+# def show_post(request, post_slug):
+#     post = get_object_or_404(Post, pk=post_slug)
+#     context = {
+#         'menu': menu,
+#         'posts': post,
+#         'title': post.title,
+#         'cat_selected': post.cat_id,
+#     }
+#
+#     return render(request, 'blog/post.html', context=context)
+
+
+class AddPage(CreateView):
+    form_class = AddPostForm
+    template_name = 'blog/addpage.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Добавление статьи'
+        context['menu'] = menu
+        return context
+
+
+# def addpage(request):
+#     if request.method == 'POST':
+#         form = AddPostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('home')
+#     else:
+#         form = AddPostForm()
+#     return render(request, 'blog/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
+
+
+def contact(request):
+    return HttpResponse("Контакты")
 
 
 def page_not_found(request, exception):
